d9070661764e00fdf44ecc59be0e44befbf93f95
giulianobre <giuliarnobre@gmail.com>
2024-07-12 09:22:08 -0300

    Criação da página de front-end para as APIs: Acessório, Cor e marca

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import MarcaView from "../views/MarcaView.vue";
+import CorView from "../views/CorView.vue";
 import LoginView from "../views/LoginView.vue";
 import LogoutView from "../views/LogoutView.vue";
 
@@ -18,6 +21,21 @@ const router = createRouter({
       component: CategoriaView,
     },
     {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
+    },
+    {
       path: "/login",
       name: "login",
       component: LoginView,
@@ -31,3 +49,4 @@ const router = createRouter({
 });
 
 export default router;
+
